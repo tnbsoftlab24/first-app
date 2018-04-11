2387375cffba01beec0594605dfa71d9b24755c0
tnbsoftlab24 <vetcho006@hotmail.com>
2018-04-11 15:26:07 +0100

    fin Active record

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,63 @@
 class ProductsController < ApplicationController
+
     def index
-        @title = "Products"
-        @product = "Tv Samsung"
-        @description = "cet article est un poste televiseur"
+        @products = Product.all
     end
 
     def new
+        @product = Product.new
     end
+    
 
     def show
     end
 
     def edit
     end
+
+    def create
+        @product = Product.new(product_params)
+    
+        respond_to do |format|
+          if @product.save
+            format.html { redirect_to @product, notice: 'Product was successfully created.' }
+            format.json { render :show, status: :created, location: @product }
+          else
+            format.html { render :new }
+            format.json { render json: @product.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
+    def update
+        respond_to do |format|
+          if @product.update(product_params)
+            format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+            format.json { render :show, status: :ok, location: @product }
+          else
+            format.html { render :edit }
+            format.json { render json: @product.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
+    def destroy
+        @product.destroy
+        respond_to do |format|
+          format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:name, :description)
+    end
+
 end 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+    belongs_to :shop
+    validates :name, presence: true
+    
+    validates :date_time
+
+    def date_time
+        if date.present? && date > Date.today
+          errors.add(:the_date, "can't be in the future")
+        end
+      end
 end
